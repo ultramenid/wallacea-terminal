911ce58daef9cfd344e5ec063af30e01cc2f67fb
ultramenid <malichamdan@gmail.com>
2025-09-17 17:54:52 +0700

    new deskripsi page

--- resources/views/frontends/aksi.blade.php
+++ resources/views/frontends/aksi.blade.php
@@ -17,7 +17,7 @@
         <div class=" border-t-4 border-t-kuning " >
             <h1 class="text-4xl font-bold mt-2 px-4">AKSI</h1>
         </div>
-        <p class="mt-10 sm:text-xl text-sm px-4 mb-20">Laman ini berisi kajian kolaborasi antara Auriga dengan peneliti dan akademisi di wilayah yang mencakup wilayah Wallacea</p>
+        <p class="mt-10 sm:text-xl text-sm px-4 mb-20">Laman ini berisi kajian kolaborasi antara Auriga dengan peneliti dan akademisi di wilayah yang mencakup wilayah Wallacea.</p>
 
             <livewire:frontend-aksi />
     </div>
